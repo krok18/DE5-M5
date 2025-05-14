cfe2cdf49751cbe5581d49049f4c54df4d5759b4
krok18 <aidenpkoutram@gmail.com>
2025-05-14 14:27:30 +0100

    added docker_calc

---
+++ docker_calc/calculator_app.py
@@ -0,0 +1,25 @@
+class Calculator:
+    
+    # Initialise the calculator with a and b, assign to self
+
+    def __init__(self, a, b): 
+        self.a = a
+        self.b = b
+
+    def do_sum(self):
+        return self.a + self.b
+    
+    def do_product(self):
+        return self.a * self.b
+    
+    def do_subtract(self):
+        return self.a - self.b
+    
+    def do_divide(self):
+        return round((self.a / self.b),2)
+    
+myCalc = Calculator(364,97)
+print(myCalc.do_product())
+
+
+
