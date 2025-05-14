a711df23ada077e84dda731920767f1a04a0c1b9
krok18 <aidenpkoutram@gmail.com>
2025-05-14 10:26:29 +0100

    renamed data_cleaner.py to app_refactored.py also created app_tests.py

--- unit_testing_demo/tests_level1.py
+++ unit_testing_demo/tests_level1.py
@@ -11,13 +11,13 @@ class TestOperations(unittest.TestCase):
         calculation = Calculator(8,2)
         self.assertEqual(calculation.do_sum(),10, "The sum is wrong (not 10)!")
 
-    def test_product(self):
-        calculation = Calculator(8,2)
-        self.assertEqual(calculation.do_product(),16, "The product is wrong (not 16)!")
-
     def test_diff(self):
         calculation = Calculator(8,2)
         self.assertEqual(calculation.do_subtract(),6, "The diff is wrong (not 6)!")   
+
+    def test_product(self):
+        calculation = Calculator(8,2)
+        self.assertEqual(calculation.do_product(),16, "The product is wrong (not 16)!")
 
     def test_division(self):
         calculation = Calculator(8,2)
