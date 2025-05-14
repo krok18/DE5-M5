109734ab3c1ba405b6b8a350dc4725184c23ae60
krok18 <aidenpkoutram@gmail.com>
2025-05-14 10:16:10 +0100

    created calculator app and testing for app

---
+++ unit_testing_demo/tests_level1.py
@@ -0,0 +1,32 @@
+import unittest
+
+from calculator_app import Calculator
+
+class TestOperations(unittest.TestCase):
+    
+    # test logic
+    ## the function must be named test_<something>
+
+    def test_sum(self):
+        calculation = Calculator(8,2)
+        self.assertEqual(calculation.do_sum(),10, "The sum is wrong (not 10)!")
+
+    def test_product(self):
+        calculation = Calculator(8,2)
+        self.assertEqual(calculation.do_product(),16, "The product is wrong (not 16)!")
+
+    def test_diff(self):
+        calculation = Calculator(8,2)
+        self.assertEqual(calculation.do_subtract(),6, "The diff is wrong (not 6)!")   
+
+    def test_division(self):
+        calculation = Calculator(8,2)
+        self.assertEqual(calculation.do_divide(),4, "The division is wrong (not 4)!")  
+
+if __name__ == '__main__':
+        unittest.main()
+
+
+
+
+
