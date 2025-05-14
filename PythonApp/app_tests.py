898b7840c460f87f37b1491fad496ba2fd4ea0f6
krok18 <aidenpkoutram@gmail.com>
2025-05-14 12:15:07 +0100

    app_tests running correctly!

--- PythonApp/app_tests.py
+++ PythonApp/app_tests.py
@@ -1,32 +1,28 @@
-import unittest
+import unittest 
 import pandas as pd
+from app_refactored import enrich_dateDuration  # Make sure this function returns a DataFrame
 
-from app_refactored import enrich_dateDuration# add functions that are needed to test
+class TestEnrichment(unittest.TestCase):
 
-# at a minimum test the following
-## check that the duration column between checkout and return is an integer
-## check that the same column is above 0
-## do any other function 
-
-class TestOperations(unittest.TestCase):
-        
     def setUp(self):
-        self.data=enrich_dateDuration(df=pd.DataFrame
-            (
-             data=[('01/01/2025', '04/01/2025'), 
-                   ('01/04/2025', '04/04/2025')], 
-             columns=['Book checkout', 'Book Returned']
-             ), 
-             colA='Book Returned',
-             colB='Book checkout'
-             )
+        # creating a test dataframe
+        self.test_df = pd.DataFrame({
+            'start_date' : ['01/01/2025', '02/01/2024', '03/01/2024'],
+            'end_date' : ['01/02/2025', '02/02/2024', '03/02/2024']
+        })
 
-    def test_durationint(self):
-         self.assertTrue(self.data.enrich_dateDuration().df['date_delta'],int,"Data type is not int")     
+        # covert the column to datetime
+        self.test_df['start_date'] = pd.to_datetime(self.test_df['start_date'], format = '%d/%m/%Y')
+        self.test_df['end_date'] = pd.to_datetime(self.test_df['end_date'], format = '%d/%m/%Y')
+
+        # apply the enrichment function
+        self.enriched_df = enrich_dateDuration(df=self.test_df, colA="start_date", colB="end_date")
+
+    def test_duration_as_int(self):
+         self.assertTrue(pd.api.types.is_integer_dtype(self.enriched_df['date_delta']), "The delta column is not an integer")
+
+    def test_duration_above_zero(self):
+         self.assertTrue((self.enriched_df['date_delta']>0).all(), "Some durations are less than 0")
 
 if __name__ == '__main__':
         unittest.main()
-
-
-
-
