a147b15ac22a3e26ad8ddb9934a10f6944e3a99f
krok18 <aidenpkoutram@gmail.com>
2025-05-14 15:52:57 +0100

    updated docker_calc app to output times table to dataframe

--- docker_calc/calculator_app.py
+++ docker_calc/calculator_app.py
@@ -1,10 +1,14 @@
+import pandas as pd
+
 class Calculator:
     
     # Initialise the calculator with a and b, assign to self
 
-    def __init__(self, a, b): 
+    def __init__(self, a=0, b=0): 
         self.a = a
         self.b = b
+
+    # Functions for the Calculator class
 
     def do_sum(self):
         return self.a + self.b
@@ -17,13 +21,42 @@ class Calculator:
     
     def do_divide(self):
         return round((self.a / self.b),2)
+    
 
-a = int(input("Please input number 'a':\n"))
+# Function for prompting the user
+## user can choose to sum two values together or visualise the times table up to 12 of an inputted number
 
-b = int(input("Please input number 'b':\n"))
+def prompt_user():
+    choice = input("Please input sum, tt, or ttdf:\n").lower()
 
-myCalc = Calculator(a,b)
-print(myCalc.do_product())
+    if choice == "sum":
+        a = int(input("Please input number 'a':\n"))
+        b = int(input("Please input number 'b':\n"))
+        myCalc = Calculator(a,b)
+        print(myCalc.do_product())
+        choice = ""
+
+    elif choice == "tt":
+        b = int(input("Please input the times table you would like to see:\n"))
+        a = 1
+        print("Please see ", b, " times table below:\n")
+        while (a < 13):
+            myCalc = Calculator(a,b)
+            print(myCalc.do_product())
+            a = a + 1
+        print("\n")
+
+    elif choice == "ttdf":
+        b = int(input("Please input the times table you would like to see:\n"))
+        a = 1
+        df = pd.DataFrame({'Position': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]})
+        df['Result'] = df['Position'] * b
+        print("Please see ", b, " times table below:\n",df)
+
+    else:
+        print('Invalid input!')
 
 
 
+if __name__ == '__main__':
+    prompt_user()
